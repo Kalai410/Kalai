9d127012ab1dcd16ffd67b5183709e00e1b88f4d
Kalai410 <137813296+Kalai410@users.noreply.github.com>
2023-07-14 14:28:42 +0530

    Add files via upload

--- odd or even.java
+++ odd or even.java
@@ -1,18 +1,24 @@
-public class MyClass {
-    public static void main(String args[]) {
-      int N=10;
-      if (N % 2 == 0) {
-      System.out.println(N + " is even ");
-      }
-      
-      else if (N == 0) {
-          System.out.println(N +"is zero");
-      }
-      
-      else {
-          System.out.println(N + "is odd");
-          
-      }
-    }
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+ public int countEvensOdds(int input1,int input2,int input3,int input4,int input5,String input6){
+  int count=0;
+  if(input1%2==0)
+   count++;
+  if(input2%2==0)
+   count++;
+  if(input3%2==0)
+   count++;
+  if(input4%2==0)
+   count++;
+  if(input5%2==0)
+   count++;
+  if(input6.equals("even"))
+   return count;
+  else
+   return 5-count;
+ }
+
 }
-      
+
